78bf6e971cf798aecf72de2f0b4a224c3f90d60b
kondurusashank <msashank@outlook.com>
2021-04-20 12:10:33 -0400

    Update ScreenAwake.py

--- ScreenAwake.py
+++ ScreenAwake.py
@@ -1,3 +1,8 @@
+# A small python script to move your mouse, press the shift key, and keep your PC awake when 
+# you're away Uses command line arguments to set the number of minutes between movements and requires Python3 or higher. 
+# Default timer is 3 minutes, but can be 1 or more.
+
+
 import pyautogui
 import time
 import sys
